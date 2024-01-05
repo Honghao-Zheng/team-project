335a6c23be586ffd55df5837ccf2d8b560e86d43
coldjim <jwr.ladley@gmail.com>
2024-01-05 08:52:15 +0000

    added aws ecr module

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,6 @@ module "vpc" {
     source = "./modules/vpc"
 }
 
-
-
 module "security" {
     source = "./modules/security"
     vpc_id = module.vpc.vpc_id
@@ -23,4 +21,11 @@ module "app_servers" {
      aws_security_group_ingress = module.security.aws_security_group_ingress
      public_subnets = module.vpc.public_subnets
      aws_security_group_app_server = module.security.aws_security_group_app_server
+}
+
+module "ecr" {
+  source           = "./modules/ecr"
+  ecr_name         = var.ecr_name
+  tags             = var.tags
+  image_mutability = var.image_mutability
 }
---
+++ terraform/variables.auto.tfvars
@@ -0,0 +1,9 @@
+tags = {
+  "Environment" = "Dev"
+}
+
+ecr_name = [
+  "teamproject/frontend"
+]
+
+image_mutability = "IMMUTABLE"
